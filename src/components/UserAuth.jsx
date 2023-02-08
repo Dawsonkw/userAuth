1697e1760d6cf76711f76eb1e67e3261e9797d15
Dawsonkw <dawsonkw@gmail.com>
2023-02-08 13:36:44 -0700

    pushing fix, something got deleted

--- src/components/UserAuth.jsx
+++ src/components/UserAuth.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { RingLoader } from 'react-spinners';
-import { BiShow } from 'react-icons/bi'
+import { BiShow } from 'react-icons/bi';
 
 
 
